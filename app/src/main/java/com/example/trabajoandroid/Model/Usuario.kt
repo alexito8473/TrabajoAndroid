e1efb48916a60c36b4fbad5b0fdcb0dfa5dbd5a6
aleja <aagualb451@g.educaand.es>
2024-02-14 19:03:50 +0100

    Mejora

--- app/src/main/java/com/example/trabajoandroid/Model/Usuario.kt
+++ app/src/main/java/com/example/trabajoandroid/Model/Usuario.kt
@@ -1,5 +1,12 @@
 package com.example.trabajoandroid.Model
-
+/**
+ * Clase que representa un objeto Usuario.
+ * @author Alejandro Aguilar Alba
+ * @since 1.0
+ * @constructor Crea un objeto Usuario con los siguientes parámetros.
+ * @param contraseña La contraseña del usuario.
+ * @param gmail Indica que gmail tiene almacenado el usuario
+ */
 class Usuario {
     private var contraseña: String
     private var gmail: String
@@ -7,11 +14,17 @@ class Usuario {
         this.gmail=gmail
         this.contraseña=contraseña
     }
-
+    /**
+     * Método que devuelve la contraseña del usuario.
+     * @return La contraseña del usuario.
+     */
     fun getContraseña(): String {
         return contraseña;
     }
-
+    /**
+     * Método que devuelve el gmail del usuario.
+     * @return El gmail del usuario.
+     */
     fun getGmail(): String {
         return gmail;
     }
--- app/src/main/java/com/example/trabajoandroid/View/CartaActivity.kt
+++ app/src/main/java/com/example/trabajoandroid/View/CartaActivity.kt
@@ -6,7 +6,12 @@ import android.os.Bundle
 import androidx.cardview.widget.CardView
 import com.example.trabajoandroid.R
 
+/**
+ * Esta clase representa la actividad de la carta
+ * Permite al usuario poder manejarse entre distintas patallas, sirve como un menu
+ */
 class CartaActivity : AppCompatActivity() {
+    // Variables que nos índica los botones de la pantalla
     private lateinit var botonFuncionLista1: CardView
     private lateinit var botonFuncionLista2: CardView
     private lateinit var botonFuncionLista3: CardView
@@ -14,12 +19,23 @@ class CartaActivity : AppCompatActivity() {
     private lateinit var botonFuncionLista5: CardView
     private lateinit var botonFuncionLista6: CardView
     private lateinit var botonFuncionVolverMenu: CardView
+    /**
+     * Método que instancia la actividad
+     * Establece los escuchas y los métodos que se van a usar
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carta)
+        // Se establece los distintos id de la activity
         establecerVariablesID()
+        // Se establece las escuchas de la activity
         establecerEscucha()
     }
+
+    /**
+     * Método que nos instancia las variables que necesidad recibir un id.
+     * Asigna a cada variable su id correspondiente.
+     */
     private fun establecerVariablesID() {
         botonFuncionLista1 = findViewById(R.id.cardMenuFuncionLista)
         botonFuncionLista2 = findViewById(R.id.cardMenuFuncionLista2)
@@ -29,6 +45,10 @@ class CartaActivity : AppCompatActivity() {
         botonFuncionLista6 = findViewById(R.id.cardMenuFuncionLista6)
         botonFuncionVolverMenu= findViewById(R.id.cardMenuGoMenu)
     }
+    /**
+     * Método que establece los escuchas de las actividades
+     * Cuando es pulsado el boton de la actividad tendra un comportamiento .
+     */
     private fun establecerEscucha() {
         botonFuncionLista1.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
